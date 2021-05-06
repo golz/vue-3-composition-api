99fa5c6b8ea9ae21a66fcd3da7b6523c9e7fd806
Goldwin Japar <golzwinsjapar@gmail.com>
2021-05-06 16:55:02 +0700

    adjust css

--- src/pages/MyTask.vue
+++ src/pages/MyTask.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-16 bg-gray-50 overflow-hidden lg:py-24">
+  <div class="py-16 overflow-hidden lg:py-24">
     <div class="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
       <div class="relative mb-10">
         <h2 class="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
